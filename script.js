be3f8a9593f888f0c6730e665f86de2f2d559309
Luke Martin <ljmart02@gmail.com>
2020-12-02 15:39:03 -0700

    Updatedjavascript

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const start = document.getElementById("btn");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const choiceA = document.getElementById("choiceA");
+const choiceB = document.getElementById("choiceB");
+const choiceC = document.getElementById("choiceC");
+const choiceD = document.getElementById("choiceD");
+
+
+var questions = document.getElementById("questionSection")
+
+
+
+function prompt () {
+    var question = document.getElementById("question")
+    var choiceA = document.getElementById("choiceA")
+    var choiceB = document.getElementById("choiceB")
+    var choiceC = document.getElementById("choiceC")
+    var choiceD = document.getElementById("choiceD")
+}
