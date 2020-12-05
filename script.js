eaf6b4ea675ff3ddd5918135f6f4ba39a8428a2e
Luke Martin <ljmart02@gmail.com>
2020-12-04 18:59:50 -0700

    Almsot working local storage

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const button = document.querySelector('button');
 const input = document.getElementById('item');
+let itemsArray = []
+if(localStorage.getItem('items')) {
+    items = JSON.parse(localStorage.getItem('items'))
+} else {
+    items = []
+}
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
+
+const liMaker = (text) => {
+    const li = document.createElement('li')
+    li.textContent = text
+    ul.appendChild(li)
+}
 
 startButton.addEventListener("click", startGame)
 var choiceA = document.getElementById("choiceA")
@@ -87,20 +101,27 @@ function selectAnswer (e) {
     
 }
 
-const liMaker = (text) => {
-    const li = document.createElement('li')
-    li.textContent = text
-    ul.appendChild(li)
-}
 
-form.addEventListener('submit', function(e) {
+
+form.addEventListener('submit', function (e) {
     e.preventDefault()
 
+    itemsArray.push(input.value)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
     liMaker(input.value)
     input.value = ''
 })
 
+data.forEach((item) => {
+    liMaker(item)
+})
 
+button.addEventListener('click', function () {
+    localStorage.clear()
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+    }
+})
 
 document.getElementsByClassName("answerChoices")
 
